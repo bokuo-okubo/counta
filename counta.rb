cdbc50530da55956e3d09fc9acc785552eb9967d
bokuo-okubo <okubo.yohei@gmail.com>
2014-09-01 19:04:45 +0900

    init

---
+++ counta.rb
@@ -0,0 +1,58 @@
+# -*- coding:utf-8-*-
+
+class Counta
+	def initialize 
+		@ary = %w(1 2 3)
+		@ary2 = []
+		printNote
+		input =""
+		@pointa = -1
+		
+		while input != "quit"
+			input = getInput
+		end
+	end
+
+	def getInput
+		input = gets.chomp.to_s
+		case input
+		when "f"
+			goForward
+		when "b"
+			goBackward
+		when "quit"
+			exit
+		else
+			printNote
+		end
+		input
+	end
+
+	def printNote
+		print "If you want to exit, type \"quit\"\n"
+		print "type \"f\" or \"b\" \!\n"
+	end
+
+
+
+	def goForward
+		if @pointa < 2
+			@pointa += 1
+		else
+			@pointa = 0
+		end
+		p @ary[@pointa]
+	end
+
+	def goBackward
+		if @pointa > 0
+			@pointa -= 1
+		else
+			@pointa = 2
+		end
+		p @ary[@pointa]
+	end
+end
+
+###########
+count = Counta.new
